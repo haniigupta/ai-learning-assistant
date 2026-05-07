427fa404f25d44e0d684b1fe5369c7cc6718db3a
guptahani75@gmail.com <guptahani75@gmail.com>
2026-05-07 16:02:21 +0530

    created all crud controller for doc

--- backend/controllers/documentController.js
+++ backend/controllers/documentController.js
@@ -8,3 +8,40 @@ import mongoose from 'mongoose';
 // @desc Upload a document
 // @route POST /api/documents/upload
 // @access Private
+
+export const uploadDocument = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        // cleanup file on error
+        if(req.file){
+            await fs.unlink(req.file.path).catch( ()=> {});
+        }
+        next(error);
+    }
+};
+
+// @desc Get all documents for a user
+// @route GET /api/documents
+// @access Private
+export const getDocuments = async (req, res, next) => {
+
+}
+// @desc Get a single document by chunks
+// @route GET /api/documents/:id
+// @access Private
+export const getDocument = async (req, res, next) => {
+
+}
+// @desc Delete a document
+// @route DELETE /api/documents/:id
+// @access Private
+export const deleteDocument = async (req, res, next) => {
+
+}
+// @desc Update a document
+// @route PUT /api/documents/:id
+// @access Private
+export const updateDocument = async (req, res, next) => {   
+
+}
