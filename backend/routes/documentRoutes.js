45d437cb6579405b7bb34f3957e737b407ee00fb
guptahani75@gmail.com <guptahani75@gmail.com>
2026-05-07 15:46:23 +0530

    created router for every doc

--- backend/routes/documentRoutes.js
+++ backend/routes/documentRoutes.js
@@ -12,4 +12,12 @@ import upload from '../config/multer.js'
 const router = express.Router();
 
 // all routes are protected
-router.use(protect)
+router.use(protect);
+
+router.post('/upload', upload.single('file'), uploadDocument);
+router.get('/', getDocuments);
+router.get('/:id', getDocumentById);
+router.put('/:id', updateDocument);
+router.delete('/:id', deleteDocument);
+
+export default router;
