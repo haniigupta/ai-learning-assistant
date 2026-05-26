220a334a475728ecff1ec903e84b7f8422a6b76a
guptahani75@gmail.com <guptahani75@gmail.com>
2026-05-26 19:57:40 +0530

    create a multer to upload a pdf

---
+++ backend/config/multer.js
@@ -0,0 +1,39 @@
+import multer from 'multer';
+import path, { parse } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const uploadDir = path.join(__dirname, '../uploads/documents');
+// Configure multer storage
+if(!fs.existsSync(uploadDir)){
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, uploadDir);
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + '-' + file.originalname);
+    }
+});
+// file filter - only PDF
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'application/pdf') {
+        cb(null, true);
+    } else {
+        cb(new Error('Invalid file type, only PDF is allowed!'), false);
+    }
+};
+// configure multer 
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits:{
+        fileSize: parseInt(process.env.MAX_FILE_SIZE) || 10485760 // default 10MB
+    }
+});
+
+export default upload;
