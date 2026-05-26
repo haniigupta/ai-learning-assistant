e77df62841d257ca10e41a06867442043310ab96
guptahani75@gmail.com <guptahani75@gmail.com>
2026-05-26 21:37:10 +0530

    created logic for deletedoc func

--- backend/controllers/documentController.js
+++ backend/controllers/documentController.js
@@ -2,7 +2,8 @@ import { stat } from 'fs';
 import Document from '../models/Document.js'
 import Flashcard from '../models/Flashcard.js'
 import Quiz from '../models/Quiz.js'
-import {chunkText, findRelevantChunks}  from '../utils/textChunker.js'
+import { extractTextFromPDF } from '../utils/pdfParser.js';
+import { chunkText } from '../utils/textChunker.js';
 import fs from 'fs/promises';
 import mongoose from 'mongoose';
 import { count } from 'console';
@@ -65,7 +66,7 @@ export const uploadDocument = async (req, res, next) => {
 //Helper function to process PDF and update document status
 const processPDF = async (documentId, filePath) => {
     try {
-        const { text } = await extraTextFromPDF(filePath);
+        const { text } = await extractTextFromPDF(filePath);
 
         // create chunks
         const chunks = chunkText(text, 500, 50);
@@ -90,7 +91,7 @@ const processPDF = async (documentId, filePath) => {
 export const getDocuments = async (req, res, next) => {
      try {
         const  documents = await Document.aggregate([
-            { $match: { userId: mongoose.Types.ObjectId(req.user._id) } },
+            { $match: { userId: new mongoose.Types.ObjectId(req.user._id) } },
             { $lookup: {
                 from: 'flashcards',
                 localField: '_id',
@@ -131,7 +132,34 @@ export const getDocuments = async (req, res, next) => {
 // @access Private
 export const getDocument = async (req, res, next) => {
     try {
-        
+        const document = await Document.findOne({ 
+            _id: req.params.id, 
+            userId: req.user._id
+        });
+        if(!document){
+            return res.status(404).json({
+                success: false,
+                message: "Document not found",
+                statusCode : 404
+            });
+        }
+        //get counts of associated flashcards and quizzes
+        const flashcardCount = await Flashcard.countDocuments({ documentId: document._id, userId: req.user._id });
+        const quizCount = await Quiz.countDocuments({ documentId: document._id, userId: req.user._id });
+
+        //update last accessed 
+        document.lastAccessed = Date.now();
+        await document.save();
+
+        // combined doc with the counts
+        const documentData = document.toObject();
+        documentData.flashcardCount = flashcardCount;
+        documentData.quizCount = quizCount;
+
+        res.status(200).json({
+            success: true,
+            data: documentData
+        });
 
     } catch (error) {
         next(error);
@@ -142,7 +170,26 @@ export const getDocument = async (req, res, next) => {
 // @access Private
 export const deleteDocument = async (req, res, next) => {
     try {
-        
+        const document = await Document.findOneAndDelete({ 
+            _id: req.params.id, 
+            userId: req.user._id   
+        });
+        if(!document){
+            return res.status(404).json({
+                success: false,
+                message: "Document not found",
+                statusCode : 404
+            });
+        }
+        // delte file from file system
+        await fs.unlink(document.filePath).catch( () => {});
+        // delete document
+        await document.deleteOne();
+
+        res.status(200).json({
+            success: true,
+            message: "Document deleted successfully"
+        });
 
     } catch (error) {
         next(error);
