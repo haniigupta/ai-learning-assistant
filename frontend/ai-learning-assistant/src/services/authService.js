09c3466dc337d05e4d390f46cfdbb9cfd0bc6e2a
guptahani75@gmail.com <guptahani75@gmail.com>
2026-05-28 14:24:42 +0530

    fix(auth): improve axios auth service and API request handling

---
+++ frontend/ai-learning-assistant/src/services/authService.js
@@ -0,0 +1,66 @@
+//Its job is to keep ALL authentication-related API calls in ONE place.
+
+import axiosInstance from "../utils/axiosInstance";
+import { API_PATHS } from "../utils/apiPath";
+
+const login = async (email, password) => {
+    try {
+        const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+            email,
+            password
+        });
+        return response.data;
+    } catch(error){
+        throw error.response?.data || { message: 'Unknown error occured'};
+    }
+};
+
+const register = async (username, email, password) => {
+    try {
+        const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+            username, 
+            email, 
+            password
+        });
+        return response.data;
+    } catch (error){
+        throw error.response?.data || { message: 'Unknown error occured'};
+    }
+};
+
+const getProfile = async () => {
+    try{
+        const response = await axiosInstance.get(API_PATHS.AUTH.GET_PROFILE);
+        return response.data;
+    } catch (error){
+        throw error.response?.data || { message: 'Unknown error occured'};
+    }
+};
+
+const updateProfile = async (userData) => {
+    try{
+        const response = await axiosInstance.put(API_PATHS.AUTH.UPDATE_PROFILE, userData);
+        return response.data;
+    } catch (error){
+        throw error.response?.data || { message: 'Unknown error occured'};
+    }
+};
+
+const changePassword = async (passwords) => {
+    try{
+        const response = await axiosInstance.get(API_PATHS.AUTH.CHANGE_PASSWORD, passwords);
+        return response.data;
+    } catch (error){
+        throw error.response?.data || { message: 'Unknown error occured'};
+    }
+};
+
+const authService = {
+    login,
+    register,
+    getProfile,
+    updateProfile,
+    changePassword,
+};
+
+export default authService;
