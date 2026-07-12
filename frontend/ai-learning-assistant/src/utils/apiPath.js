9a3fbb65c965f345774f092e6fa9873827c02602
guptahani75@gmail.com <guptahani75@gmail.com>
2026-07-12 16:47:23 +0530

    changed base url

--- frontend/ai-learning-assistant/src/utils/apiPath.js
+++ frontend/ai-learning-assistant/src/utils/apiPath.js
@@ -1,5 +1,5 @@
-//export const BASE_URL = "https://ai-learning-assistant-bvxq.onrender.com";
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://ai-learning-assistant-bvxq.onrender.com";
+//export const BASE_URL = "http://localhost:8000";
 export const API_PATHS = {
   AUTH: {
     REGISTER: "/api/auth/register",
